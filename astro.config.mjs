2143448a38b1ee28e1d074359a7dc93859c78a2a
Liam Beckman <lbeckman314@gmail.com>
2025-12-01 18:56:25 -0800

    feat: Add initial site search via Pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { remarkAlert } from "remark-github-blockquote-alert";
 import custom from './src/styles/codedark.js';
 import mdx from '@astrojs/mdx';
+import pagefind from "astro-pagefind";
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import { remarkAlert } from "remark-github-blockquote-alert";
 
 // Config
 // https://astro.build/config
 export default defineConfig({
   site: 'https://liambeckman.com',
 
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(), 
+    pagefind(),
+    sitemap(),
+  ],
 
   // https://eikowagenknecht.de/posts/use-github-alerts-admonitions-callouts-in-astro/
   markdown: {
--- src/content/notes/Example.md
+++ src/content/notes/Example.md
@@ -4,3 +4,18 @@ date: 2025-11-01T00:00:00.000Z
 ---
 
 Example
+
+> [!NOTE]  
+> Highlights information that users should take into account, even when skimming.
+
+> [!TIP]
+> Optional information to help a user be more successful.
+
+> [!IMPORTANT]  
+> Crucial information necessary for users to succeed.
+
+> [!WARNING]  
+> Critical content demanding immediate user attention due to potential risks.
+
+> [!CAUTION]
+> Negative potential consequences of an action.
