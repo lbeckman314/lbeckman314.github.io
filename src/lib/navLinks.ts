9b3a97abed833a699380c9df63b507a6e555e396
Liam Beckman <lbeckman314@gmail.com>
2026-08-10 17:24:51 -0700

    chore: Add icons to header
    Signed-off-by: Liam Beckman <lbeckman314@gmail.com>

---
+++ src/lib/navLinks.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    href: "/",
+    label: "Home",
+    icon: `<path d="M5 12l-2 0l9 -9l9 9l-2 0"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/>`,
+  },
+  {
+    href: "/notes",
+    label: "Notes",
+    icon: `<path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z"/><path d="M9 7l6 0"/><path d="M9 11l6 0"/><path d="M9 15l4 0"/>`,
+  },
+  {
+    href: "/projects",
+    label: "Projects",
+    icon: `<path d="M7 8l-4 4l4 4"/><path d="M17 8l4 4l-4 4"/><path d="M14 4l-4 16"/>`,
+  },
+  {
+    href: "/about",
+    label: "About",
+    icon: `<path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"/><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"/>`,
+  },
+];
